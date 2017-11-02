fe84c9f22c68f9cefc49d478bfe7194ee5ed6330
Olivia Hugger <olivia@fastmail.com>
2017-11-02 16:21:05 +0100

    basic workshopper setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const cc = require('workshopper-adventure')({
+  appDir: __dirname,
+  languages: ['en'],
+  header: require('workshopper-adventure/default/header'),
+  footer: require('workshopper-adventure/default/footer')
+})
+
+module.exports = cc
