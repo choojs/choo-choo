0e97c63b53b39828ccc067cd467cab603f677b8b
Olivia Hugger <olivia@fastmail.com>
2017-11-03 13:02:18 +0100

    finish the exercise

--- exercises/a_cute_beginning/exercise.js
+++ exercises/a_cute_beginning/exercise.js
@@ -2,21 +2,39 @@ let exercise = require('workshopper-exercise')()
 const filecheck = require('workshopper-exercise/filecheck')
 const express = require('express')
 const browserify = require('browserify-middleware')
+const getPort = require('get-port')
+const isHtml = require('is-html')
+const got = require('got')
 const path = require('path')
+const out = require('../../lib/out')
 
 exercise = filecheck(exercise)
 
 exercise.addSetup(function (mode, cb) {
+  out.info(this.__('setup.starting'))
   this.server = express()
   this.server.use(express.static(process.cwd()))
   this.server.use('/bundle.js', browserify(path.join(process.cwd(), this.args[0])))
-  this.server.listen(8000, () => {
-    console.log('up')
+  getPort().then(port => {
+    this.port = port
+    this.server.listen(port, () => {
+      out.info(this.__('setup.started', {port: this.port}))
+      setTimeout(cb, 1000)
+    })
   })
 })
 
-exercise.addCleanup(function (mode, passed, cb) {
-  this.server.close(callback)
+exercise.addProcessor(function (mode, cb) {
+  got(`http://localhost:${this.port}`).then(res => {
+    out.pass(this.__('pass.response'))
+    if (isHtml(res.body)) {
+      out.pass(this.__('pass.content_ok'))
+      cb()
+    } else {
+      out.error(this.__('fail.not_html'))
+      cb(new Error(this.__('fail.not_html')))
+    }
+  })
 })
 
 module.exports = exercise
