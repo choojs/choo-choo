ed369f6a034e649a104c7d9e745c72f48cad1efb
Olivia Hugger <olivia@fastmail.com>
2017-11-03 13:05:53 +0100

    naming consistencies

--- exercises/a_cute_beginning/exercise.js
+++ exercises/a_cute_beginning/exercise.js
@@ -6,19 +6,19 @@ const getPort = require('get-port')
 const isHtml = require('is-html')
 const got = require('got')
 const path = require('path')
-const out = require('../../lib/out')
+const log = require('../../lib/console')
 
 exercise = filecheck(exercise)
 
 exercise.addSetup(function (mode, cb) {
-  out.info(this.__('setup.starting'))
+  log.info(this.__('setup.starting'))
   this.server = express()
   this.server.use(express.static(process.cwd()))
   this.server.use('/bundle.js', browserify(path.join(process.cwd(), this.args[0])))
   getPort().then(port => {
     this.port = port
     this.server.listen(port, () => {
-      out.info(this.__('setup.started', {port: this.port}))
+      log.info(this.__('setup.started', {port: this.port}))
       setTimeout(cb, 1000)
     })
   })
@@ -26,12 +26,12 @@ exercise.addSetup(function (mode, cb) {
 
 exercise.addProcessor(function (mode, cb) {
   got(`http://localhost:${this.port}`).then(res => {
-    out.pass(this.__('pass.response'))
+    log.pass(this.__('pass.response'))
     if (isHtml(res.body)) {
-      out.pass(this.__('pass.content_ok'))
+      log.pass(this.__('pass.content_ok'))
       cb()
     } else {
-      out.error(this.__('fail.not_html'))
+      log.fail(this.__('fail.not_html'))
       cb(new Error(this.__('fail.not_html')))
     }
   })
