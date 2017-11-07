adb08efff897522471a90a62248ef19b40c325ff
Olivia Hugger <olivia@fastmail.com>
2017-11-07 11:47:27 +0100

    extract bundle server

--- exercises/a_cute_beginning/exercise.js
+++ exercises/a_cute_beginning/exercise.js
@@ -1,29 +1,12 @@
 let exercise = require('workshopper-exercise')()
 const filecheck = require('workshopper-exercise/filecheck')
-const express = require('express')
-const browserify = require('browserify-middleware')
-const getPort = require('get-port')
 const isHtml = require('is-html')
 const got = require('got')
-const path = require('path')
 const log = require('../../lib/console')
+const bundleServer = require('../../lib/bundleServer')
 
 exercise = filecheck(exercise)
-
-exercise.addSetup(function (mode, cb) {
-  log.info(this.__('setup.starting'))
-  this.server = express()
-  this.server.use(express.static(process.cwd()))
-  this.server.use('/bundle.js', browserify(path.join(process.cwd(), this.args[0])))
-  getPort().then(port => {
-    this.port = port
-    this.server.listen(port, () => {
-      log.info(this.__('setup.started', {port: this.port}))
-      if (mode !== 'run')
-        setTimeout(cb, 1000)
-    })
-  })
-})
+exercise = bundleServer(exercise)
 
 exercise.addVerifyProcessor(function (cb) {
   got(`http://localhost:${this.port}`).then(res => {
