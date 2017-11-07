62e82fbccc170ee9d5223ccdaf51fe2d95780558
Olivia Hugger <olivia@fastmail.com>
2017-11-07 12:05:52 +0100

    add some comments

--- exercises/a_cute_beginning/exercise.js
+++ exercises/a_cute_beginning/exercise.js
@@ -5,9 +5,15 @@ const got = require('got')
 const log = require('../../lib/console')
 const bundleServer = require('../../lib/bundleServer')
 
+// Check if the file passed in as argument exists...
 exercise = filecheck(exercise)
+
+// Set up our bundle serving server...
 exercise = bundleServer(exercise)
 
+// Add a processor that only runs on `choo-choo verify` that checks if the
+// bundle server returns anything valid HTML, meaning that the directory has
+// been successfully downloaded
 exercise.addVerifyProcessor(function (cb) {
   got(`http://localhost:${this.bundleServerPort}`).then(res => {
     log.pass(this.__('pass.response'))
