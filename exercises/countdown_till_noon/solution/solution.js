34a97fdd245166377690086ec581a604ac5d7c28
Olivia Hugger <olivia@fastmail.com>
2017-11-10 19:39:12 +0100

    add correct solution for exercise 2

--- exercises/countdown_till_noon/solution/solution.js
+++ exercises/countdown_till_noon/solution/solution.js
@@ -1,14 +1,31 @@
 const choo = require('choo')
 const html = require('choo/html')
+const verify = require('choo-choo/verify')
 
 const app = choo()
 
-app.route('/', () => {
-  return html`
-    <body>
-      hello world!
-    </body>
-  `
+app.use(function (state, emitter) {
+  state.clicked = 0
+
+  emitter.on('inc', function () {
+    state.clicked += 1
+    if (state.clicked === 5) {
+      verify(state)
+    }
+  })
 })
 
+function indexView (state, emit) {
+  function onClick () {
+    emit('inc')
+  }
+  
+  return html`
+    <body>
+      <button onclick=${onClick}>click me!</button>
+    </body>
+  `
+}
+
+app.route('/', indexView)
 app.mount('body')
