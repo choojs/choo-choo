0869b89f6ccea934ec6e0f7accaa26b1fe51ac46
Olivia Hugger <olivia@fastmail.com>
2017-11-10 16:51:18 +0100

    extract resServer

--- exercises/countdown_till_noon/exercise.js
+++ exercises/countdown_till_noon/exercise.js
@@ -5,26 +5,19 @@ const bodyParser = require('body-parser')
 const ora = require('ora')
 const log = require('../../lib/console')
 const bundleServer = require('../../lib/bundleServer')
+const resServer = require('../../lib/resServer')
 
 exercise = filecheck(exercise)
 
 exercise = bundleServer(exercise)
 
-exercise.addSetup(function (mode, cb) {
-  this.resServer = express()
-  this.spinner = ora(this.__('setup.waiting_verification'))
-  this.resServer.use(bodyParser.json({
-    type: 'text/plain'
-  }))
+resServer(exercise, function (cb) {
   this.resServer.post('/response', (req, res) => {
     res.end()
     if (req.body.passed === true) {
       this.spinner.succeed(this.__('pass.verification_ok'))
       cb()
     }
-  })
-  this.resServer.listen(8574, () => {
-    this.spinner.start()
   })
 })
 
